ae527874ad241dc2afdea6cbb26b1e35c3a318d8
ahmedkamer123 <qamerahmed54@gmail.com>
2024-10-30 01:08:31 +0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,14 @@
+let img = document.querySelector('.imag');
+let container = document.querySelector('.container');
+
+function phones (phone ) {
+    img.src = phone;
+}
+
+function colors (color) {
+    container.Style.colors
+
+
+    
+}
+
